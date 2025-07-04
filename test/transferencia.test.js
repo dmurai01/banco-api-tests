f630fdd481c572cc9c6e0bee4e62229079445b3b
Daniela Murai <dany.murai@gmail.com>
2025-07-04 10:11:30 -0300

    adicionando testes de transferencia

---
+++ test/transferencia.test.js
@@ -0,0 +1,64 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Transferências', () => {
+    describe('POST /trasnferencias', () => {
+        // R1 - Valor mínimo para transferência é R$ 10,00
+        it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
+            // Capturar token
+            const respostaLogin = await request('http://localhost:3000')
+                // supertest
+                .post('/login')
+                .set('Content-type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+            const token = respostaLogin.body.token
+
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+
+                    'contaOrigem': 1,
+                    'contaDestino': 2,
+                    'valor': 11,
+                    'token': ''
+
+                })
+            expect(resposta.status).to.equal(201);
+            console.log(resposta.body)
+
+        });
+        it('Deve retornar falha com 422 quando o valor da transferência for abaixo que R$10,00', async () => {
+            // Capturar token
+            const respostaLogin = await request('http://localhost:3000')
+                // supertest
+                .post('/login')
+                .set('Content-type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+            const token = respostaLogin.body.token
+
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+
+                    'contaOrigem': 1,
+                    'contaDestino': 2,
+                    'valor': 8,
+                    'token': ''
+
+                })
+            expect(resposta.status).to.equal(422);
+            console.log(resposta.body)
+        });
+    });
+
+});
