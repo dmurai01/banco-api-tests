b5ac546833ba8c9fd6a01d17d97e6dadcc4e93c3
Daniela Murai <dany.murai@gmail.com>
2025-07-11 15:56:22 -0300

    incluindo testes de erro login e mais testes de transferencias GET

--- test/login.test.js
+++ test/login.test.js
@@ -10,7 +10,7 @@ const postLogin = require('../fixtures/postLogin.json')
 // utilizando mocha para estruturar e executar os testes
 describe('Login', () => {
     describe('POST /login', () => {
-        it('Deve retornar 200 com token em string quando usar credenciais válidas', async () => {
+        it('Deve retornar sucesso 200 com token em string quando usar credenciais válidas', async () => {
             const bodyLogin = {...postLogin}
             const resposta = await request(process.env.BASE_URL)
                 // supertest
@@ -20,6 +20,30 @@ describe('Login', () => {
             // chai
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
+        });
+
+        it('Deve retornar falha 400 quando não enviar usuário ou senha', async () => {
+            const bodyLogin = {...postLogin}
+            bodyLogin.senha = null
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/login')
+                .set('Content-type', 'application/json')
+                .send(bodyLogin)
+            expect(resposta.status).to.equal(400)
+            expect(resposta.body.error).to.equal('Usuário e senha são obrigatórios.')
+        });
+
+        it('Deve retornar falha 401 quando enviar usuário ou senha inválidos', async () => {
+            const bodyLogin = {...postLogin}
+            bodyLogin.senha = '123'
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/login')
+                .set('Content-type', 'application/json')
+                .send(bodyLogin)
+            expect(resposta.status).to.equal(401)
+            expect(resposta.body.error).to.equal('Usuário ou senha inválidos.')
         });
     });
 });
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -11,9 +11,9 @@ describe('Transferências', () => {
         token = await obterToken('julio.lima', '123456')
     });
 
-    describe('POST /trasnferencias', () => {
-        // R1 - Valor mínimo para transferência é R$ 10,00
-        it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
+    describe('POST /transferencias', () => {
+
+        it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00 e menor que R$ 5000,00', async () => {
             // clonando postTransferencia (copia superficial, somente objetos com propriedades de primeiro nível - sem subniveis)
             const bodyTransferencias = { ...postTransferencias }
 
@@ -25,6 +25,73 @@ describe('Transferências', () => {
             expect(resposta.status).to.equal(201);
         });
 
+        it('Deve retornar sucesso com 201 quando o valor da transferência for acima de R$ 5000,00 e envio de token', async () => {
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 10000
+            bodyTransferencias.token = '123456'
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(bodyTransferencias)
+            expect(resposta.status).to.equal(201);
+        });
+
+        it('Deve retornar falha com 400 quando forem enviados parâmentros inválidos', async () => {
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 'Doze'
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(bodyTransferencias)
+            expect(resposta.status).to.equal(400);
+        });
+
+        it('Deve retornar falha com 401 quando enviada autenticação de token incorreta', async () => {
+            const bodyTransferencias = { ...postTransferencias }
+            token = '123'
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(bodyTransferencias)
+            expect(resposta.status).to.equal(401);
+        });
+
+        it('Deve retornar falha com 401 quando o valor da transferencia for acima de R$ 5000,00 e sem envio de token', async () => {
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 5005
+            bodyTransferencias.token = '111'
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(bodyTransferencias)
+
+            expect(resposta.status).to.equal(401)
+            expect(resposta.body.error).to.equal('Autenticação necessária para transferências acima de R$5.000,00.')
+        });
+
+
+        it('Deve retornar falha 422 quando o Saldo for insuficiente ', async () => {
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 100000
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(bodyTransferencias)
+            expect(resposta.status).to.equal(422);
+
+        });
+
+
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo que R$10,00', async () => {
             const bodyTransferencias = { ...postTransferencias }
             bodyTransferencias.valor = 7
@@ -35,6 +102,18 @@ describe('Transferências', () => {
                 .set('Authorization', `Bearer ${token}`)
                 .send(bodyTransferencias)
             expect(resposta.status).to.equal(422);
+        });
+    });
+
+    describe('GET /transferencias', () => {
+        it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=10')
+                .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(10)
+            expect(resposta.body.transferencias).to.have.lengthOf(10)
         });
     });
 
@@ -51,18 +130,30 @@ describe('Transferências', () => {
             expect(resposta.body.valor).to.be.a('number')
             expect(resposta.body.valor).to.equal(15.00)
         });
-    });
 
-    describe('GET /transferencias', () => {
-        it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+        it('Deve retornar falha 401 quando token não estiver autenticado', async () => {
+            token = null
             const resposta = await request(process.env.BASE_URL)
-                .get('/transferencias?page=1&limit=10')
+                .get('/transferencias/99999')
                 .set('Authorization', `Bearer ${token}`)
 
-            expect(resposta.status).to.equal(200)
-            expect(resposta.body.limit).to.equal(10)
-            expect(resposta.body.transferencias).to.have.lengthOf(10)    
+            expect(resposta.status).to.equal(401)
+        });
+
+        it('Deve retornar falha 404 quando o consultar id de transferencia não existente', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/99999')
+                .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(404)
         });
     });
+
+    // describe('PUT /transferencias/{id}', () => {
+    //     it('Deve retornar sucesso 204 quando todos os dados forem enviados corretamente', () => {
+
+
+    //     });
+    // });
 
 });
